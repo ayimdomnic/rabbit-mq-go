4cfee6e447036eddd6318f8ac482368ea618b365
Odhiambo Dormnic <ayimdomnic@gmail.com>
2020-01-20 21:32:37 +0300

    fix on the amqlib service

--- http.go
+++ http.go
@@ -57,6 +57,105 @@ type RabbitMQ struct {
 	done    chan error
 }
 
+func (r *RabbitMQ) Publish(exchange, key string, deliverymode, priority uint8, body string) (err error) {
+	err = r.channel.Publish(exchange, key, false, false,
+		amqp.Publishing{
+			Headers:         amqp.Table{},
+			ContentType:     "text/plain",
+			ContentEncoding: "",
+			DeliveryMode:    deliverymode,
+			Priority:        priority,
+			Body:            []byte(body),
+		},
+	)
+	if err != nil {
+		log.Printf("[amqp] publish message error: %s\n", err)
+		return err
+	}
+	return nil
+}
+
+func (r *RabbitMQ) DeclareExchange(name, typ string, durable, autodelete, nowait bool) (err error) {
+	err = r.channel.ExchangeDeclare(name, typ, durable, autodelete, false, nowait, nil)
+	if err != nil {
+		log.Printf("[amqp] declare exchange error: %s\n", err)
+		return err
+	}
+	return nil
+}
+
+func (r *RabbitMQ) DeleteExchange(name string) (err error) {
+	err = r.channel.ExchangeDelete(name, false, false)
+	if err != nil {
+		log.Printf("[amqp] delete exchange error: %s\n", err)
+		return err
+	}
+	return nil
+}
+
+func (r *RabbitMQ) DeclareQueue(name string, durable, autodelete, exclusive, nowait bool) (err error) {
+	_, err = r.channel.QueueDeclare(name, durable, autodelete, exclusive, nowait, nil)
+	if err != nil {
+		log.Printf("[amqp] declare queue error: %s\n", err)
+		return err
+	}
+	return nil
+}
+
+func (r *RabbitMQ) DeleteQueue(name string) (err error) {
+	// TODO: other property wrapper
+	_, err = r.channel.QueueDelete(name, false, false, false)
+	if err != nil {
+		log.Printf("[amqp] delete queue error: %s\n", err)
+		return err
+	}
+	return nil
+}
+
+func (r *RabbitMQ) BindQueue(queue, exchange string, keys []string, nowait bool) (err error) {
+	for _, key := range keys {
+		if err = r.channel.QueueBind(queue, key, exchange, nowait, nil); err != nil {
+			log.Printf("[amqp] bind queue error: %s\n", err)
+			return err
+		}
+	}
+	return nil
+}
+
+func (r *RabbitMQ) UnBindQueue(queue, exchange string, keys []string) (err error) {
+	for _, key := range keys {
+		if err = r.channel.QueueUnbind(queue, key, exchange, nil); err != nil {
+			log.Printf("[amqp] unbind queue error: %s\n", err)
+			return err
+		}
+	}
+	return nil
+}
+
+func (r *RabbitMQ) ConsumeQueue(queue string, message chan []byte) (err error) {
+	deliveries, err := r.channel.Consume(queue, "", true, false, false, false, nil)
+	if err != nil {
+		log.Printf("[amqp] consume queue error: %s\n", err)
+		return err
+	}
+	go func(deliveries <-chan amqp.Delivery, done chan error, message chan []byte) {
+		for d := range deliveries {
+			message <- d.Body
+		}
+		done <- nil
+	}(deliveries, r.done, message)
+	return nil
+}
+
+func (r *RabbitMQ) Close() (err error) {
+	err = r.conn.Close()
+	if err != nil {
+		log.Printf("[amqp] close error: %s\n", err)
+		return err
+	}
+	return nil
+}
+
 // TODO::Organize http requests to the amqp server
 func main() {
         http.HandleFunc("/exchange", ExchangeHandler)
