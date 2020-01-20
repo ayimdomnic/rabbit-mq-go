ed66b54f21b3923c1a341120682f7e7427ed8c51
Odhiambo Dormnic <ayimdomnic@gmail.com>
2020-01-20 21:29:02 +0300

    Entity for the payload mapping, like interfaces if you ask me

--- http.go
+++ http.go
@@ -19,6 +19,44 @@ func init() {
         flag.parse()
 }
 
+type MessageEntity struct {
+        Exchange     string `json:"exchange"`
+        Key          string `json:"key"`
+        DeliveryMode uint8  `json:"deliverymode"`
+        Priority     uint8  `json:"priority"`
+        Body         string `json:"body"`
+}
+
+type ExchangeEntity struct {
+	Name       string `json:"name"`
+	Type       string `json:"type"`
+	Durable    bool   `json:"durable"`
+	AutoDelete bool   `json:"autodelete"`
+	NoWait     bool   `json:"nowait"`
+}
+
+type QueueEntity struct {
+	Name       string `json:"name"`
+	Durable    bool   `json:"durable"`
+	AutoDelete bool   `json:"autodelete"`
+	Exclusive  bool   `json:"exclusive"`
+	NoWait     bool   `json:"nowait"`
+}
+
+type QueueBindEntity struct {
+	Queue    string   `json:"queue"`
+	Exchange string   `json:"exchange"`
+	NoWait   bool     `json:"nowait"`
+	Keys     []string `json:"keys"` // bind/routing keys
+}
+
+// RabbitMQ Operate Wrapper
+type RabbitMQ struct {
+	conn    *amqp.Connection
+	channel *amqp.Channel
+	done    chan error
+}
+
 // TODO::Organize http requests to the amqp server
 func main() {
         http.HandleFunc("/exchange", ExchangeHandler)
